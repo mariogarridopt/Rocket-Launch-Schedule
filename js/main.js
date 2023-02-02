1f8c54c3d43a3a2c28e7c0a4bd2de3f135ae7ff4
Mário Garrido <1191102+mariogarridopt@users.noreply.github.com>
2023-02-02 20:28:41 +0000

    fix interface jumps (#33)

--- js/main.js
+++ js/main.js
@@ -4,17 +4,17 @@ if (isDataInCache("launch")) {
     drawLaunchHtml(launch);
 } else {
     const launchesAPI =
-    "https://ll.thespacedevs.com/2.0.0/launch/upcoming/?limit=10&mode=detailed";
+        "https://ll.thespacedevs.com/2.0.0/launch/upcoming/?limit=10&mode=detailed";
 
     _ajaxrequest(launchesAPI, function (d) {
-        if(d != false) {
+        if (d != false) {
             updateCachedData("launch", d);
             drawLaunchHtml(d);
         }
     },
-    function (e) {
-        console.error(e);
-    });
+        function (e) {
+            console.error(e);
+        });
 }
 
 // Draw Astronauts
@@ -23,15 +23,15 @@ if (isDataInCache("astronaut")) {
     drawAstronautHtml(astronaut);
 } else {
     const astronautsAPI =
-    "https://ll.thespacedevs.com/2.2.0/astronaut/?in_space=true&is_human=true";
+        "https://ll.thespacedevs.com/2.2.0/astronaut/?in_space=true&is_human=true";
 
     _ajaxrequest(astronautsAPI, function (d) {
-        if(d != false) {
+        if (d != false) {
             updateCachedData("astronaut", d);
             drawAstronautHtml(d);
         }
     },
-    function (e) {
-        console.error(e);
-    });
+        function (e) {
+            console.error(e);
+        });
 }
